690d3075560dd125f14eaba68c224e85cf442eb5
Aicha Mbongo <zindamoyen2@gmail.com>
2024-01-21 18:29:45 +0300

    style(navbar): implement color change on scroll
    Added JavaScript functionality to dynamically change the background color of
    the navbar when the page is scrolled. Improved user experience by providing
    visual feedback as the user navigates through the page.

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ function toggleText(cardId) {
       moreText.style.display = "inline";
     }
   }
+
+// Get the navbar element
+var navbar = document.getElementById("myNavbar");
+
+// Listen for the scroll event
+window.onscroll = function () {
+  // If the scroll position is greater than a certain value, add the "scrolled" class
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    // Otherwise, remove the "scrolled" class
+    navbar.classList.remove("scrolled");
+  }
+};
